6baf97b2388342683948ef68e43265bfd384db4d
chedger <unyboman@gmail.com>
2017-08-07 09:57:30 -0700

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function modify_qty(val) {
     if (new_qty < 0) {
         new_qty = 0;
     }
-   if (new_qty > 5) {
+   if (new_qty == 5) {
         new_qty = 0; 
         document.getElementById("demo").innerHTML = result;
 }
