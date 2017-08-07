6b80d20dc9293109243fb06b0f39a83b3346ccfd
chedger <unyboman@gmail.com>
2017-08-07 10:18:26 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
+var qty = 0; 
+
+// use document.getElementById() to set the value of
+// the input box here
+
+
 function modify_qty(val) {
-    var qty = document.getElementById('qty').value;
-    var new_qty = parseInt(qty,10) + val;
-    
+    var new_qty = qty + val;
     
     if (new_qty < 0) {
         new_qty = 0;
     }
     
-    document.getElementById('qty').value = new_qty;
     return new_qty;
 }
 
