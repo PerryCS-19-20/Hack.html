534bd5f25929307f60a59d395750c95036147359
chedger <unyboman@gmail.com>
2017-08-07 10:03:47 -0700

    Update script.js

--- script.js
+++ script.js
@@ -6,12 +6,12 @@ function modify_qty(val) {
     if (new_qty < 0) {
         new_qty = 0;
     }
-   if (qty > 5) {
-        new_qty = 0; 
+   if (new_qty > 5) {
+         
         
         var str = "Free Web Building Tutorials!";
     var result = str.link("https://www.w3schools.com");
-       document.getElementById("demo").innerHTML = result;
+       document.getElementById('up').innerHTML = result;
 }
     
     document.getElementById('qty').value = new_qty;
