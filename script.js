b1895d70dbd06d7bc0f05b629073c94bdc2102fe
chedger <unyboman@gmail.com>
2017-08-07 10:23:07 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,1 @@
-var qty = 0; 
-
-// use document.getElementById() to set the value of
-// the input box here
-
-
-function modify_qty(val) {
-    var new_qty = qty + val;
-    
-    if (new_qty < 0) {
-        new_qty = 0;
-    }
-    
-    return new_qty;
-}
 
